d7a4f9a8011e234cf880cdeb3894f84db1362863
Tatiana Churkina <sharonova_t@inbox.ru>
2026-02-17 11:01:24 +0300

    добавлена минимальная валидация формы

---
+++ src/scripts/data.js
@@ -0,0 +1,17 @@
+import image from "../images/Karachaevsk.jpg";
+
+const defaultCards = [
+    {
+        name: "Карачаевск",
+        place: image,
+    },
+    {
+        name: "Гора Эльбрус",
+        place: image,
+    },
+    {
+        name: "Домбай",
+        place: image,
+    },
+];
+export {defaultCards};
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -3,34 +3,35 @@ import "../styles/globals.css";
 import "../styles/style.css";
 import "../styles/variables.css";
 
-
-import image from "../images/Karachaevsk.jpg";
-
-const defaultCards = [
-    {
-        name: "Карачаевск",
-        place: image,
-    },
-    {
-        name: "Гора Эльбрус",
-        place: image,
-    },
-    {
-        name: "Домбай",
-        place: image,
-    },
-];
+import {defaultCards} from "./data";
 
 const addDialog = document.getElementById("add-dialog");
 const addSubmit = document.getElementById("add-dialog__submit");
-const placeName = document.getElementById("place-name");
-const placeUrl = document.getElementById("place-url");
+const addPlaceForm = document.forms.add;
 const cards = document.querySelector(".elements");
-addSubmit.addEventListener("click", (e) => {
+addPlaceForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    addPlace(placeName.value, placeUrl.value);
+    addPlace(addPlaceForm.name.value, addPlaceForm.url.value);
     addDialog.close();
+    addPlaceForm.reset();
+    setSubmitButtonState(false);
 });
+
+addPlaceForm.addEventListener("input", (e) => {
+    const isFormValid = addPlaceForm.name.value.length > 0 && addPlaceForm.url.value.length > 0;
+    setSubmitButtonState(isFormValid);
+})
+
+
+function setSubmitButtonState(isFormValid) {
+    if (isFormValid) {
+        addSubmit.removeAttribute('disabled');
+        addSubmit.classList.remove('input__btn_disabled');
+    } else {
+        addSubmit.setAttribute('disabled', true);
+        addSubmit.classList.add('input__btn_disabled');
+    }
+}
 
 function likeButtonClick(event) {
     event.preventDefault();
