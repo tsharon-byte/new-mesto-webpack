ff8c4d560b984496b59723da204d850bd07fbc75
Tatiana Churkina <sharonova_t@inbox.ru>
2026-02-17 11:55:44 +0300

    исправлено окно редактирования профиля

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -8,9 +8,10 @@ import {defaultCards} from "./data";
 const addDialog = document.getElementById("add-dialog");
 const profileDialog = document.getElementById("profile-dialog");
 const addSubmit = document.getElementById("add-dialog__submit");
+const profileSubmit = document.getElementById("profile-dialog__submit");
 const editButton = document.querySelector(".profile__edit-button");
-const profileName=document.querySelector("#profile__name");
-const profileDescription=document.querySelector("#profile__description");
+const profileName = document.querySelector("#profile__name");
+const profileDescription = document.querySelector("#profile__description");
 const addPlaceForm = document.forms.add;
 const profileForm = document.forms.profile;
 const cards = document.querySelector(".elements");
@@ -19,31 +20,30 @@ addPlaceForm.addEventListener("submit", (e) => {
     addPlace(addPlaceForm.name.value, addPlaceForm.url.value);
     addDialog.close();
     addPlaceForm.reset();
-    setSubmitButtonState(false);
+    setSubmitButtonState(addSubmit, false);
 });
 
 document.addEventListener("click", (e) => {
     if (e.target.id === "add-dialog") {
         addDialog.close();
-    }
-    else if (e.target.id === "profile-dialog") {
+    } else if (e.target.id === "profile-dialog") {
         profileDialog.close();
     }
 })
 
 addPlaceForm.addEventListener("input", (e) => {
     const isFormValid = addPlaceForm.name.value.length > 0 && addPlaceForm.url.value.length > 0;
-    setSubmitButtonState(isFormValid);
+    setSubmitButtonState(addSubmit,isFormValid);
 })
 
 
-function setSubmitButtonState(isFormValid) {
+function setSubmitButtonState(button, isFormValid) {
     if (isFormValid) {
-        addSubmit.removeAttribute('disabled');
-        addSubmit.classList.remove('input__btn_disabled');
+        button.removeAttribute('disabled');
+        button.classList.remove('input__btn_disabled');
     } else {
-        addSubmit.setAttribute('disabled', true);
-        addSubmit.classList.add('input__btn_disabled');
+        button.setAttribute('disabled', true);
+        button.classList.add('input__btn_disabled');
     }
 }
 
@@ -77,8 +77,21 @@ function init() {
 
 editButton.addEventListener('click', (event) => {
     profileDialog.showModal();
-    profileForm.name.value=profileName.textContent;
-    profileForm.description.value=profileDescription.textContent;
+    profileForm.name.value = profileName.textContent;
+    profileForm.description.value = profileDescription.textContent;
+});
+
+profileForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    profileName.textContent = profileForm.name.value;
+    profileDescription.textContent = profileForm.description.value;
+    profileDialog.close();
+});
+
+
+profileForm.addEventListener("input", (e) => {
+    const isFormValid = profileForm.name.value.length > 0 && profileForm.description.value.length > 0;
+    setSubmitButtonState(profileSubmit,isFormValid);
 })
 
 init();
